fe585b721029663d8cfa51a2fe010a90d5d113f4
Marton Fonagy <fonagymarci@gmail.com>
2023-07-21 16:04:18 +0200

    Goofing around

--- app/src/main/java/com/fonagyma/rikiki/MainActivity.kt
+++ app/src/main/java/com/fonagyma/rikiki/MainActivity.kt
@@ -3,13 +3,34 @@ package com.fonagyma.rikiki
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fonagyma.rikiki.ui.theme.RikikiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +43,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AgeSlider(modifier = Modifier.fillMaxSize(), maxAge = 10)
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true,
+    device = "id:pixel_4"
+)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WelcomeScreenPreview() {
+    RikikiTheme {
+        WelcomeScreen(modifier = Modifier.fillMaxSize())
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RikikiTheme {
-        Greeting("Android")
+fun WelcomeScreen(modifier: Modifier) {
+   Box(
+       modifier = modifier.padding(10.dp),
+       contentAlignment = Alignment.Center
+   ){
+       Text(text = "Let's play Rikiki!", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+   }
+}
+
+@Composable
+fun AgeSlider(modifier : Modifier, maxAge: Int = 99) {
+    var currentAge by remember { mutableStateOf(10) }
+
+    Column( 
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Choose your age!", fontSize = 16.sp, modifier = Modifier.padding(5.dp))
+        Text(text = "$currentAge", fontSize = 26.sp,fontWeight = FontWeight.Bold, modifier = Modifier.padding(5.dp,24.dp,5.dp,5.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            IconButton(onClick = { currentAge--}) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "remove")
+            }
+            IconButton(onClick = { currentAge++ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "add")
+            }
+        }
     }
 }
